939301aa0ea125498be12130f6a5dcb3ab73da0e
Mark Mesander <mark_mesander@hotmail.com>
2023-09-07 14:30:06 +0200

    add hello() and variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
 public class Main {
     public static void main(String[] args) {
+        int number = 12;
+        int input1 = 4;
+        int input2 = 8;
+        String name = "Mark Mesander";
+
+        hello();
+    }
+
+    private static void hello() {
         System.out.println("Hello world!");
     }
+
+    private static void positiveOrNegative(){
+
+    }
 }
+
+
+
