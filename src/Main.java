e902fa7b0e0670939e73ea54b489f3c6fe4f579e
Mark Mesander <mark_mesander@hotmail.com>
2023-09-07 14:36:45 +0200

    add positiveOrZeroOrNegative method

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        int number = -12;
+        int number = 0;
         int input1 = 4;
         int input2 = 8;
         String name = "Mark Mesander";
 
         hello();
         positiveOrNegative(number);
+        positiveOrZeroOrNegative(number);
     }
 
     private static void hello() {
@@ -18,6 +19,16 @@ public class Main {
             System.out.println("The number is positive!");
         } else {
             System.out.println("The number is negative!");
+        }
+    }
+
+    private static void positiveOrZeroOrNegative(int number) {
+        if (number < 0) {
+            System.out.println("The number is negative!");
+        } else if (number == 0) {
+            System.out.println("The number is zero!");
+        } else {
+            System.out.println("The number is positive");
         }
     }
 }
